83987ecc579c2fe932f7a83296fe29a6fdba78a5
Po <chenjunfeng@ethstorage.io>
2023-12-13 09:28:51 +0100

    wasm-compile

--- wasm-compile/readme.md
+++ wasm-compile/readme.md
@@ -1,4 +1,5 @@
 ```
 cargo install wasm-pack
 wasm-pack build --release
+/root/now/zkWasm/target/release/delphinus-cli -k 22 --function zkmain --wasm pkg/wasm_compile_bg.wasm dry-run --public 3:i64 
 ```
--- wasm-compile/src/lib.rs
+++ wasm-compile/src/lib.rs
@@ -1,6 +1,7 @@
 extern "C" {
     pub fn wasm_input(is_public: u32) -> u64;
     pub fn require(cond: bool);
+    pub fn wasm_dbg(val:u64);
 }
 
 use wasm_bindgen::prelude::*;
@@ -9,6 +10,7 @@ use wasm_bindgen::prelude::*;
 pub fn zkmain() -> i64 {
     unsafe {
         let input = wasm_input(1);
+        wasm_dbg(input as u64);
         return 2 * input as i64;
     }
 }
