871434f77898ead0936f17197f492157d21b1dc9
Po <wwwcjf163@163.com>
2025-10-06 12:51:10 +0800

    add closure

--- concepts/10_closure/src/main.rs
+++ concepts/10_closure/src/main.rs
@@ -1,6 +1,21 @@
-mod closure_trait;
-mod closure;
+// mod closure;
+// mod closure_trait;
+
+#[no_mangle]
+fn add_fn(x: i32) -> i32 {
+    x + 1
+}
 
 fn main() {
-    println!("Hello, world!");
+    let a = 10;
+
+    // 1. 函数指针调用
+    let f: fn(i32) -> i32 = add_fn;
+    let r1 = f(a);
+
+    // 2. 闭包调用
+    let closure = |x| x + 1;
+    let r2 = closure(a);
+
+    println!("r1 = {}, r2 = {}", r1, r2);
 }
