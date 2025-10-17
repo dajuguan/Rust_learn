8c0c244da57b40ade5ac4d8fb5fd80efbac64480
Po <wwwcjf163@163.com>
2025-10-17 17:41:13 +0800

    add box pointer address

--- concepts/01_heap_stack/src/pointer.rs
+++ concepts/01_heap_stack/src/pointer.rs
@@ -1,5 +1,10 @@
 fn foo() {}
 
+#[derive(Clone)]
+struct Foo {
+    data: i32,
+}
+
 #[test]
 fn pointer() {
     let s = "hello".to_string();
@@ -8,5 +13,22 @@ fn pointer() {
     println!("STACK: {:p}", &s);
     println!("HEAP: {:p}", &*s);
     println!("HEAP: {:p}, {:p}", data, &*data);
-    println!("TEXT: {:p}", foo as *const());
+    println!("TEXT: {:p}", foo as *const ());
+}
+
+#[test]
+fn pointer_box() {
+    let foo = Foo { data: 0 };
+    println!("STACK struct: {:p}", &foo);
+    let fooBox = Box::new(foo.clone());
+    println!("STACK box: {:p}", &fooBox);
+    println!("HEAP box: {:p}", &*fooBox);
+
+    let fooBox1 = Box::new(foo);
+    println!("STACK box1: {:p}", &fooBox1);
+    println!("HEAP box1: {:p}", &*fooBox1);
+
+    let fooBox2 = Box::new(Foo { data: 1 });
+    println!("STACK box2: {:p}", &fooBox2);
+    println!("HEAP box2: {:p}", &*fooBox2);
 }
