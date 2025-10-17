19e579dae292d2f59e11e8f9eba5cc89425cc074
Po <wwwcjf163@163.com>
2025-10-17 19:19:08 +0800

    add box pointer size

--- concepts/01_heap_stack/src/pointer.rs
+++ concepts/01_heap_stack/src/pointer.rs
@@ -2,7 +2,13 @@ fn foo() {}
 
 #[derive(Clone)]
 struct Foo {
+    // rust will alloc 32 bytes (not 4 bytes) when using Box::new
     data: i32,
+}
+
+struct FooLarge {
+    data: i64,
+    s1: i128,
 }
 
 #[test]
@@ -19,16 +25,34 @@ fn pointer() {
 #[test]
 fn pointer_box() {
     let foo = Foo { data: 0 };
-    println!("STACK struct: {:p}", &foo);
-    let fooBox = Box::new(foo.clone());
-    println!("STACK box: {:p}", &fooBox);
+    let fooClone = foo.clone();
+    println!(
+        "STACK struct: foor: {:p}, foo clone: {:p}, size of fooPtr: {}",
+        &foo,
+        &fooClone,
+        std::mem::size_of_val(&foo)
+    );
+    let fooBox = Box::new(fooClone);
+
+    println!(
+        "STACK box: {:p}, size of boxPtr: {}",
+        &fooBox,
+        std::mem::size_of_val(&fooBox)
+    );
     println!("HEAP box: {:p}", &*fooBox);
 
     let fooBox1 = Box::new(foo);
-    println!("STACK box1: {:p}", &fooBox1);
-    println!("HEAP box1: {:p}", &*fooBox1);
 
     let fooBox2 = Box::new(Foo { data: 1 });
+    let fooBox3 = Box::new(FooLarge { data: 1, s1: 2 });
+    let fooBox4 = Box::new(FooLarge { data: 1, s1: 2 });
+    let fooBox5 = Box::new(FooLarge { data: 1, s1: 2 });
+    println!("STACK box1: {:p}", &fooBox1);
     println!("STACK box2: {:p}", &fooBox2);
+
+    println!("HEAP box1: {:p}", &*fooBox1);
     println!("HEAP box2: {:p}", &*fooBox2);
+    println!("HEAP box3: {:p}", &*fooBox3);
+    println!("HEAP box4: {:p}", &*fooBox4);
+    println!("HEAP box5: {:p}", &*fooBox5);
 }
