05114e20503d10571dfd1d9d9fbb82a4c8e989f4
Po <wwwcjf163@163.com>
2025-11-11 17:49:43 +0800

    add mem take

--- concepts/06_ownership/src/vec_owner.rs
+++ concepts/06_ownership/src/vec_owner.rs
@@ -4,3 +4,26 @@ fn test_vec_move() {
     let v1 = std::mem::take(&mut str_vec[1]);
     println!("moved v1:{:?}, vec at 1:{:?}", v1, str_vec[1]);
 }
+
+#[test]
+fn test_vec_addr_non_reallocation() {
+    // 创建一个 vec 并填充一些元素
+    let mut v = vec![1, 2, 3];
+
+    // 获取 v 中第一个元素的地址
+    let v_first_addr = &v[0] as *const i32;
+
+    // 调用 std::mem::take 将 v 的内容转移到 taken
+    let taken = std::mem::take(&mut v);
+
+    // 获取 taken 中第一个元素的地址
+    let taken_first_addr = &taken[0] as *const i32;
+
+    // 输出地址
+    println!("Address of before v's first element: {:?}", v_first_addr);
+    println!("Address of taken's first element: {:?}", taken_first_addr);
+
+    // 打印 taken 和 v 的内容:发现take没有重新分配内存
+    println!("Taken: {:?}", taken); // [1, 2, 3]
+    println!("v after take: {:?}", v); // []
+}
