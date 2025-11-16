e146b636cb2f63ed2326c25df04508fba8716830
Po <wwwcjf163@163.com>
2025-11-16 16:07:47 +0800

    pin: key design rational for get_unchecked_mut

--- concepts/12_future_pin/src/pin_test.rs
+++ concepts/12_future_pin/src/pin_test.rs
@@ -101,10 +101,12 @@ impl<Ptr: DerefMut> MyPinCorrect<Ptr> {
 }
 
 impl<'a, T> MyPinCorrect<&'a mut T> {
-    unsafe fn get_unchecked_mut_wrong(&mut self) -> &mut T {
+    // get_unchecked_mut_wrong only provides a reference that lives for as long as the borrow of the Pin, not the lifetime of the reference contained in the Pin.
+    unsafe fn get_unchecked_mut_wrong_lifetime(&mut self) -> &mut T {
         self.ptr
     }
 
+    // This method allows turning the Pin into a reference with the same lifetime as the reference it wraps.
     const unsafe fn get_unchecked_mut(self) -> &'a mut T {
         self.ptr
     }
@@ -200,77 +202,37 @@ mod tests {
     }
 
     #[test]
-    fn test_self_referential_mypin_correct_ub() {
+    fn test_self_referential_mypin_correct_shorter_lifetime() {
         // instance is moved from move_create_issues{} to instance, must use function, or rust may optimize it away and the moved instance might not be gced, which cause the test passed uncorrectly.
         let mut instance = SelfReferentialPin::new(String::from("A!"));
         instance.init();
-        let instance = unsafe { MyPinCorrect::new_unchecked(&mut instance) };
-        instance.get_data_via_pointer("inst");
-        let mut ins_a = instance;
-        // nothing changed, because we can only move MyPinCorrect (ptr to SelfReferential), not the inner SelfReferential which is shadowed by MyPinCorrect instance.
-        ins_a.get_data_via_pointer("inst after move to a");
+        let mut instance = unsafe { MyPinCorrect::new_unchecked(&mut instance) };
 
-        let mut ins_b = SelfReferentialPin::new(String::from("B!"));
-        ins_b.init();
-        let mut ins_b = unsafe { MyPinCorrect::new_unchecked(&mut ins_b) };
-        ins_b.get_data_via_pointer("inst b");
-
-        let mut a_mut = ins_a.as_mut();
-        let mut b_mut = ins_b.as_mut();
-        // from semantics, a_mut(注意视角是a_mut，而不是inst_a) is moved in unsafe which violates the pin guarantee.
-        unsafe {
+        let a_mut = unsafe {
             // UB(undefined behavior)
-            let a_mut = a_mut.get_unchecked_mut_wrong();
-            let b_mut = b_mut.get_unchecked_mut_wrong();
-            std::mem::swap(a_mut, b_mut);
+            let mut a_mut = instance.as_mut();
+            // a mut is only as only as instance.as_mut() borrow, so the returned reference can't outlive it. Thus, rust doesn't allow it.
+            let a_mut = a_mut.get_unchecked_mut_wrong_lifetime();
+            a_mut
         };
 
-        // 从语义上来说，a_mut的内容不允许被改变，因为a_mut被pin住了，但是此处我们获取了a_mut的可变引用，并改变了a的内容，此时a_mut的pin保证被破坏了(而a_mut的所有者可能还不知道这回事儿，以为）。
         a_mut.get_data_via_pointer("inst after unsafe get mut");
-
-        // from semantics, ins_a as_mut create a new MyPinCorrect<&mut T>, so the previous ins_a still holds the promise that it won't be moved, it's the new MyPinCorrect that moved the value.
-        unsafe {
-            // UB
-            let a_mut = a_mut.get_unchecked_mut();
-            let b_mut = b_mut.get_unchecked_mut();
-            std::mem::swap(a_mut, b_mut);
-        };
-        // 由于a_mut被move了，所以不会出现为定义行为，a_mut的所有者知道Pinned的值被改变了。
-        // a_mut.get_data_via_pointer("inst after unsafe get mut");
-
-        ins_a.get_data_via_pointer("inst a after derefmut swap");
-        ins_b.get_data_via_pointer("inst b after derefmut swap");
     }
 
     #[test]
-    fn test_self_referential_pin_ub() {
+    fn test_self_referential_mypin_correct_correct_lifetime() {
         // instance is moved from move_create_issues{} to instance, must use function, or rust may optimize it away and the moved instance might not be gced, which cause the test passed uncorrectly.
         let mut instance = SelfReferentialPin::new(String::from("A!"));
         instance.init();
-        let instance = unsafe { Pin::new_unchecked(&mut instance) };
-        instance.get_data_via_pointer("inst");
-        let mut ins_a = instance;
-        ins_a.get_data_via_pointer("inst after move to a");
+        let mut instance = unsafe { MyPinCorrect::new_unchecked(&mut instance) };
 
-        let mut ins_b = SelfReferentialPin::new(String::from("B!"));
-        ins_b.init();
-        let mut ins_b = unsafe { Pin::new_unchecked(&mut ins_b) };
-        ins_b.get_data_via_pointer("inst b");
-
-        let a_mut = ins_a.as_mut();
-        let b_mut = ins_b.as_mut();
-
-        // This function is unsafe. You must guarantee that you will never move the data out of the mutable reference you receive when you call this function,
-        // so that the invariants on the Pin type can be upheld.
-        unsafe {
-            // UB
-            // 即使是拿到了Pin指向值的的可变引用Pin(一个新的pin)，也需要开发者自己保证不会移动被Pin住的值(因为这个Pin指向的值被move后，其他相关Pin的实例也会受到影响)，否则会违反Pin的语义。
+        let a_mut = unsafe {
+            // UB(undefined behavior)
+            let mut a_mut = instance.as_mut();
             let a_mut = a_mut.get_unchecked_mut();
-            let b_mut = b_mut.get_unchecked_mut();
-            std::mem::swap(a_mut, b_mut);
+            a_mut
         };
 
-        ins_a.get_data_via_pointer("inst a after derefmut swap");
-        ins_b.get_data_via_pointer("inst b after derefmut swap");
+        a_mut.get_data_via_pointer("inst after unsafe get mut");
     }
 }
