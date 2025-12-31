23d779947c75fc2fa93caaabae43012c893c3702
Po <wwwcjf163@163.com>
2025-12-31 12:04:20 +0800

    GAT: add more comments

--- concepts/11_generic_trait/src/gat.rs
+++ concepts/11_generic_trait/src/gat.rs
@@ -1,13 +1,22 @@
-/* References:
-
+/* GAT核心:
+GAT:使关联类型成为类型构造器。
 GAT 本质上解决的是同一类问题：关联类型本身也需要带参数（生命周期 / 类型， 即关联类型不能是一个‘定值’，而必须是一个‘类型函数），而这些参数必须和 Self 的借用或泛型约束绑定。
 1. 生命周期类问题: 返回的类型需要“借用 self”，且该借用的生命周期只能和某一次调用绑定，而不是和 Self 绑定。比如Async, 下面的LendingIterator。
 2. 返回的关联类型自己再吃一个泛型参数： 比如reth db或者PointerFamily例子。
+
+
+References:
+- [The push for GATs stabilization](https://blog.rust-lang.org/2021/08/03/GATs-stabilization-push/)
+- [GAT RFC](https://github.com/rust-lang/rfcs/blob/master/text/1598-generic_associated_types.md)
+- [GAT explaination by nikomatsakis](https://www.youtube.com/watch?v=JwG-Wa7dOBU)
+- [Async impl Trait in traits requires GATs](https://smallcultfollowing.com/babysteps/blog/2019/10/26/async-fn-in-traits-are-hard/)
+- [GATs: Decide whether to have defaults for where Self: 'a](https://github.com/rust-lang/rust/issues/87479)
 */
 
 use std::{io::Error, ops::Deref, sync::Arc};
 
-/* Why GAT is a must? e.g., why iterator is enough?
+/* =====================================================
+Why GAT is a must? e.g., why iterator is enough?
 It can't express the trait bound on Item and next<'a>.
  */
 struct WindowIter<'a> {
@@ -27,7 +36,8 @@ impl<'a> Iterator for WindowIter<'a> {
     }
 }
 
-/* Why where Self:x is a must
+/* =====================================================
+Why where Self:x is a must
 trait LendingIteratorNoClause {
     // we must ensure self type outlive 'a, so where clause is a must.
     type Item<'x>; // surpose it's correct, just ignore the warning
@@ -57,7 +67,8 @@ fn boom<'x>(x: &'x i32) {
 }
  */
 
-/* Correct version
+/* =====================================================
+Correct version
  */
 trait LendingIterator {
     // we must ensure self type outlive 'a, so where clause is a must.
@@ -84,7 +95,8 @@ impl<'a> LendingIterator for WindowIter<'a> {
     }
 }
 
-/* Reth db中的教科书级别GAT使用
+/* =====================================================
+Reth db中的教科书级别GAT使用
  */
 
 /// Read only transaction
@@ -123,12 +135,13 @@ pub trait Deserialize<'de>: Sized {
     fn deserialize<D>(deserializer: D) -> Result<Self, Error>;
 }
 
-/* 常见的FamilyPointer GAT: 关联类型generic over实现类型(Self) + 类型参数。
+/*=====================================================
+常见的FamilyPointer GAT: 关联类型generic over实现类型(Self) + 类型参数。
 不论Arc, Box都能作为Generic，同时其中的字段本身又是一个Generic的类型。
  */
 
 pub trait PointerFamily {
-    type Pointer<T>: Deref<Target = T>;
+    type Pointer<T>: Deref<Target = T>; // f(t) => sometype(t)
     fn new<T>(value: T) -> Self::Pointer<T>;
 }
 
@@ -136,8 +149,18 @@ pub trait PointerFamily {
 struct ArcFamily;
 
 impl PointerFamily for ArcFamily {
-    type Pointer<T> = Arc<T>;
+    type Pointer<T> = Arc<T>; // like f(t) => Arc(t)
     fn new<T>(value: T) -> Self::Pointer<T> {
+        Arc::new(value)
+    }
+}
+
+// 注意: 虽然这么写也没问题，但是语义就不对了，我们希望是为任意的类型T，创建Arc<T>；
+// 但是下面的写法只能为某个实例化的Arc<T>类型，比如Arc<i32>，创建Arc<i32>。
+// Arc<T>表面看起来是泛型，但是实际上是实例化的泛型了，T会被单态化；而ArcFamily的语义是关联类型是类型函数，不是具体的类型。
+impl<T> PointerFamily for Arc<T> {
+    type Pointer<P> = Arc<P>;
+    fn new<P>(value: P) -> Self::Pointer<P> {
         Arc::new(value)
     }
 }
@@ -146,7 +169,7 @@ impl PointerFamily for ArcFamily {
 struct BoxFamily;
 
 impl PointerFamily for BoxFamily {
-    type Pointer<T> = Box<T>;
+    type Pointer<T> = Box<T>; // like f(t) => box(t)
     fn new<T>(value: T) -> Self::Pointer<T> {
         Box::new(value)
     }
@@ -159,9 +182,11 @@ struct Widget<P: PointerFamily> {
 }
 
 type ArcWidget = Widget<ArcFamily>;
+type ArcAWidget<T> = Widget<Arc<T>>;
 type BoxWidget = Widget<BoxFamily>;
 
-/* PointerFamily without GAT, will be more complex.
+/* =====================================================
+PointerFamily without GAT, will be more complex.
  */
 pub trait PointerFamilyNoGAT<T> {
     type Pointer: Deref<Target = T>;
