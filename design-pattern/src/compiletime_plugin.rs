90bc935a9dc1395a0bab7b484c8400337a64cc15
Po <wwwcjf163@163.com>
2026-01-04 16:54:15 +0800

    design pattern: refine compiletime plugin

--- design-pattern/src/compiletime_plugin.rs
+++ design-pattern/src/compiletime_plugin.rs
@@ -1,4 +1,5 @@
 mod node_core {
+    use std::fmt::Debug;
 
     pub trait DBProvider {
         fn get(&self, key: &str) -> String;
@@ -6,7 +7,7 @@ mod node_core {
     }
 
     pub trait NodeTypes {
-        type Block;
+        type Block: Debug + Copy + From<u32>;
         type ChainSpec;
         type Provider: DBProvider;
     }
@@ -15,18 +16,77 @@ mod node_core {
         provider: N::Provider,
     }
 
-    pub trait NodeComponents<T: NodeTypes>: Send + Sync {
-        fn validate_block(&self, block_number: u64) -> bool;
-        fn handle_block(&self, block_number: u64);
-
-        fn bn(&self) -> T::Block;
+    impl<N: NodeTypes> BuildContext<N> {
+        pub fn build_provider(provider: N::Provider) -> Self {
+            BuildContext { provider }
+        }
     }
 
-    pub trait NodeComponetsBuilder<Node: NodeTypes>: Send {
-        type Components: NodeComponents<Node>;
+    pub trait EvmExecutor<N: NodeTypes>: Send + Sync {
+        fn handle_block(&self, block_number: N::Block);
+    }
+
+    pub trait MockConsensus<N: NodeTypes>: Send + Sync {
+        fn validate_block(&self, block_number: N::Block) -> bool;
+    }
+
+    pub trait NodeComponentsTrait<T: NodeTypes>: Send + Sync {
+        type Consensus: MockConsensus<T>;
+        type Evm: EvmExecutor<T>;
+
+        fn consensus(&self) -> &Self::Consensus;
+        fn evm(&self) -> &Self::Evm;
+    }
+
+    pub struct NodeComponents<Consensus, EVM> {
+        consensus: Consensus,
+        evm: EVM,
+    }
+
+    impl Default for NodeComponents<(), ()> {
+        fn default() -> Self {
+            Self {
+                consensus: (),
+                evm: (),
+            }
+        }
+    }
+
+    impl<Node, Consensus, EVM> NodeComponentsTrait<Node> for NodeComponents<Consensus, EVM>
+    where
+        Node: NodeTypes,
+        Consensus: MockConsensus<Node>,
+        EVM: EvmExecutor<Node>,
+    {
+        type Consensus = Consensus;
+        type Evm = EVM;
+
+        fn consensus(&self) -> &Self::Consensus {
+            &self.consensus
+        }
+
+        fn evm(&self) -> &Self::Evm {
+            &self.evm
+        }
+    }
+
+    impl<Consensus, EVM> NodeComponents<Consensus, EVM> {
+        pub fn build_consensus<CB>(self, consensus: CB) -> NodeComponents<CB, EVM> {
+            let Self { consensus: _, evm } = self;
+            NodeComponents { consensus, evm }
+        }
+
+        pub fn build_executor<EB>(self, evm: EB) -> NodeComponents<Consensus, EB> {
+            let Self { consensus, evm: _ } = self;
+            NodeComponents { consensus, evm }
+        }
+    }
+
+    pub trait NodeComponetsBuilder<Node: NodeTypes> {
+        type Components: NodeComponentsTrait<Node>;
 
         /// returns the created components.
-        fn build_components(&self, ctx: BuildContext<Node>) -> Self::Components;
+        fn build_components(&self, ctx: &BuildContext<Node>) -> Self::Components;
     }
 
     pub struct NodeAdapter<N: NodeTypes, C> {
@@ -37,14 +97,18 @@ mod node_core {
     impl<N, C> NodeAdapter<N, C>
     where
         N: NodeTypes,
-        C: NodeComponents<N>,
+        C: NodeComponentsTrait<N>,
     {
-        /// Process a block: validate + handle
-        fn process_block(&self, block_number: u64) {
-            if self.components.validate_block(block_number) {
-                self.components.handle_block(block_number);
+        /// simulate backfill
+        fn back_fill(&self) {
+            println!("historical sync done!")
+        }
+        /// Simulate engine api for processing a block: validate + handle
+        fn process_block(&self, block_number: N::Block) {
+            if self.components.consensus().validate_block(block_number) {
+                self.components.evm().handle_block(block_number);
             } else {
-                println!("Block {} is invalid!", block_number);
+                println!("Block {:?} is invalid!", block_number);
             }
         }
 
@@ -52,8 +116,10 @@ mod node_core {
             Self { types, components }
         }
 
+        /// inherit tons of core logics for free!
         pub fn launch_node(&self) {
-            self.process_block(0);
+            self.back_fill();
+            self.process_block(0.into());
         }
     }
 
@@ -84,29 +150,49 @@ mod eth {
         type Provider = MockProvider;
     }
 
-    /// ETH components implementing consensus + block handler
-    pub struct EthComponents;
-
-    impl NodeComponents<EthTypes> for EthComponents {
-        fn validate_block(&self, block_number: u64) -> bool {
+    pub struct EthConsensus;
+    impl MockConsensus<EthTypes> for EthConsensus {
+        fn validate_block(&self, block_number: <EthTypes as NodeTypes>::Block) -> bool {
             println!("[ETH] Validating block {}", block_number);
             block_number % 2 == 0 // dummy rule
         }
+    }
 
-        fn handle_block(&self, block_number: u64) {
+    pub struct EthEvm;
+    impl EvmExecutor<EthTypes> for EthEvm {
+        fn handle_block(&self, block_number: <EthTypes as NodeTypes>::Block) {
             println!("[ETH] Handling block {}", block_number);
-        }
-
-        fn bn(&self) -> <EthTypes as NodeTypes>::Block {
-            1
         }
     }
 
-    pub struct EthNodeBuilder;
-    impl NodeComponetsBuilder<EthTypes> for EthNodeBuilder {
-        type Components = EthComponents;
-        fn build_components(&self, ctx: BuildContext<EthTypes>) -> Self::Components {
-            EthComponents {}
+    /// ETH components implementing consensus + block handler
+    pub struct EthNode {
+        ctx: BuildContext<EthTypes>,
+    }
+
+    impl EthNode {
+        pub fn new() -> Self {
+            Self {
+                ctx: BuildContext::build_provider(MockProvider),
+            }
+        }
+
+        pub fn ctx(&self) -> &BuildContext<EthTypes> {
+            &self.ctx
+        }
+    }
+    impl EthNode {
+        pub fn components(&self) -> NodeComponents<EthConsensus, EthEvm> {
+            NodeComponents::default()
+                .build_consensus(EthConsensus {})
+                .build_executor(EthEvm {})
+        }
+    }
+
+    impl NodeComponetsBuilder<EthTypes> for EthNode {
+        type Components = NodeComponents<EthConsensus, EthEvm>;
+        fn build_components(&self, ctx: &BuildContext<EthTypes>) -> Self::Components {
+            self.components()
         }
     }
 }
@@ -120,34 +206,54 @@ mod bsc {
     #[derive(Clone)]
     pub struct BscTypes;
     impl NodeTypes for BscTypes {
-        type Block = String;
-        type ChainSpec = u64;
+        type Block = u64;
+        type ChainSpec = String;
         type Provider = MockProvider;
     }
 
-    /// BSC components implementing consensus + block handler
-    pub struct BscComponents;
-
-    impl NodeComponents<BscTypes> for BscComponents {
-        fn validate_block(&self, block_number: u64) -> bool {
+    pub struct BscConsensus;
+    impl MockConsensus<BscTypes> for BscConsensus {
+        fn validate_block(&self, block_number: <BscTypes as NodeTypes>::Block) -> bool {
             println!("[BSC] Validating block {}", block_number);
             block_number % 2 == 0 // dummy rule
         }
+    }
 
-        fn handle_block(&self, block_number: u64) {
+    pub struct BscEvm;
+    impl EvmExecutor<BscTypes> for BscEvm {
+        fn handle_block(&self, block_number: <BscTypes as NodeTypes>::Block) {
             println!("[BSC] Handling block {}", block_number);
-        }
-
-        fn bn(&self) -> <BscTypes as NodeTypes>::Block {
-            "2".to_string()
         }
     }
 
-    pub struct EthNodeBuilder;
-    impl NodeComponetsBuilder<BscTypes> for EthNodeBuilder {
-        type Components = BscComponents;
-        fn build_components(&self, ctx: BuildContext<BscTypes>) -> Self::Components {
-            BscComponents {}
+    /// ETH components implementing consensus + block handler
+    pub struct BscNode {
+        ctx: BuildContext<BscTypes>,
+    }
+
+    impl BscNode {
+        pub fn new() -> Self {
+            Self {
+                ctx: BuildContext::build_provider(MockProvider),
+            }
+        }
+
+        pub fn ctx(&self) -> &BuildContext<BscTypes> {
+            &self.ctx
+        }
+    }
+    impl BscNode {
+        pub fn components(&self) -> NodeComponents<BscConsensus, BscEvm> {
+            NodeComponents::default()
+                .build_consensus(BscConsensus {})
+                .build_executor(BscEvm {})
+        }
+    }
+
+    impl NodeComponetsBuilder<BscTypes> for BscNode {
+        type Components = NodeComponents<BscConsensus, BscEvm>;
+        fn build_components(&self, ctx: &BuildContext<BscTypes>) -> Self::Components {
+            self.components()
         }
     }
 }
@@ -161,11 +267,13 @@ mod tests {
         use super::node_core::*;
 
         // Compose ETH node
-        let eth_node = NodeAdapter::new(EthTypes, EthComponents);
+        let node = EthNode::new();
+        let eth_node = NodeAdapter::new(EthTypes, node.build_components(node.ctx()));
         eth_node.launch_node();
 
         // Compose BSC node
-        let bsc_node = NodeAdapter::new(BscTypes, BscComponents);
+        let node = BscNode::new();
+        let bsc_node = NodeAdapter::new(BscTypes, node.build_components(node.ctx()));
         bsc_node.launch_node();
     }
 }
